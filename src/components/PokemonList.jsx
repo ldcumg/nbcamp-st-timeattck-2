378010dd70dc485a3d143bb7b38978bbf8fbd8d3
WonbinLee <wonbin145@gmail.com>
2024-08-28 13:00:24 +0900

    유효성 검사 추가

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,10 +1,19 @@
 import PokemonCard from "./PokemonCard";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addPokemon } from "../redux/slices/selectPokemonSlice";
 
 const PokemonList = ({ pokemonlist }) => {
   const dispatch = useDispatch();
+  const selectedPokemons = useSelector((state) => state.slectPokemon);
   const onAddPokemon = (target) => {
+    if (selectedPokemons.includes(target)) {
+      alert("이미 존재하는 포켓몬");
+      return;
+    }
+    if (selectedPokemons.length >= 6) {
+      alert("최대 6마리까지");
+      return;
+    }
     dispatch(addPokemon(target));
   };
   return (
