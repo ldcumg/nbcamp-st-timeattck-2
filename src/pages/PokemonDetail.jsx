0f94df7f811fcf56dbbdce99e95d545d1ffb3533
ldcumg <ldcumg@naver.com>
2024-08-28 13:12:43 +0900

    Update PokemonDetail.jsx

--- src/pages/PokemonDetail.jsx
+++ src/pages/PokemonDetail.jsx
@@ -4,9 +4,16 @@ import MOCK_DATA from "../mock";
 const PokemonDetail = () => {
   const [serchParams] = useSearchParams();
   const targetId = Number(serchParams.get("id"));
+  const navigate = useNavigate();
 
   const targetPokemon = MOCK_DATA.find((pokemon) => pokemon.id === targetId);
-  return <div>{targetPokemon.korean_name}</div>;
+  return (
+    <div>
+      <img src={targetPokemon.img_url} />
+      <h4>{targetPokemon.korean_name}</h4>
+      <button onClick={() => navigate("/dex")}>뒤로가기</button>
+    </div>
+  );
 };
 
 export default PokemonDetail;
